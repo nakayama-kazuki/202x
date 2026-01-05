33cb415068e5a5ef471f6c2657b85e7b1576c6b9
nakayama-kazuki <pj.corridor@gmail.com>
2026-01-06 06:14:48 +0900

    fix

--- pj-corridor.net/personalitytest/lambda/handler.php
+++ pj-corridor.net/personalitytest/lambda/handler.php
@@ -1,11 +1,44 @@
 <?php
 
-$event = json_decode(stream_get_contents(STDIN), true);
+function request(): array {
+	if (getenv('AWS_LAMBDA_RUNTIME_API')) {
+		$ev = json_decode(stream_get_contents(STDIN), true);
+		return [
+			'method'  => $ev['requestContext']['http']['method'] ?? '',
+			'path'	  => $ev['rawPath'] ?? '',
+			'query'   => $ev['queryStringParameters'] ?? [],
+			'headers' => $ev['headers'] ?? [],
+			'cookies' => $ev['cookies'] ?? [],
+			'body'	  => $ev['body'] ?? ''
+		];
+	} else {
+		return [
+			'method'  => $_SERVER['REQUEST_METHOD'] ?? '',
+			'path'	  => parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
+			'query'   => $_GET,
+			'headers' =>  apache_request_headers(),
+			'cookies' => $_COOKIE,
+			'body'	  => file_get_contents('php://input')
+		];
+	}
+}
 
-echo json_encode([
-	'statusCode' => 200,
-	'headers' => ['Content-Type' => 'text/plain'],
-	'body' => print_r($event, true)
-]);
+function response($in_status, $in_headers, $in_body): void {
+	if (getenv('AWS_LAMBDA_RUNTIME_API')) {
+		print json_encode([
+			'statusCode' => $in_status,
+			'headers' => $in_headers,
+			'body' => $in_body
+		]);
+	} else {
+		http_response_code($in_status);
+		foreach ($in_headers as $key => $value) {
+			header("{$key}: {$value}");
+		}
+		print $in_body;
+	}
+}
+
+response(200, ['Content-Type' => 'text/plain'], print_r(request(), true));
 
 ?>
